c3233a1a0ca34f91470cf4ed473497f9e5f5caf7
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-05-13 19:38:27 -0500

    Formatted measurements

--- app/models/Measurement.scala
+++ app/models/Measurement.scala
@@ -1,10 +1,46 @@
 package models
 
 case class Measurement(
-	val id: Int,
-	val status: ContributionStatus,
-	val playerId: Int,
-	val measurable: Measurable,
-	val measurement: Double,
-	val source: Int
-)
+  val id: Int,
+  val status: ContributionStatus,
+  val playerId: Int,
+  val measurable: Measurable,
+  val measurement: Double,
+  val source: Int
+) {
+  def display = {
+    if ((measurable == Measurables.height || measurable == Measurables.wingspan) && measurement > 12 ) {
+      math.floor(measurement / 12) + "' " + (measurement % 12) + getFractionEntity(measurement) + "\""
+    } else if (measurable.unit == Inches) {
+      measurement + getFractionEntity(measurement) + "\""
+    } else if (measurable.unit == Seconds) {
+      BigDecimal(measurement).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+    } else {
+      measurement
+    }
+  }
+
+  def getFractionEntity(measurement: Double) = {
+    val frac = measurement - math.floor(measurement)
+    if (frac == 0.125) {
+      "&#8539;"
+    } else if (frac == 0.25) {
+      "&#188;"
+    } else if (frac == 0.375) {
+      "&#188;"
+    } else if (frac == 0.5) {
+      "&#188;"
+    } else if (frac == 0.625) {
+      "&#188;"
+    } else if (frac == 0.75) {
+      "&#188;"
+    } else if (frac == 0.875) {
+      "&#188;"
+    } else if (frac != 0) {
+      BigDecimal(frac).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+        .toDouble.toString.drop(1).take(3);
+    } else {
+      ""
+    }
+  }
+}
