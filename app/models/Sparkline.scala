8e50261cd3369481133a55869be83f0a274f6b36
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-07-07 21:21:29 -0400

    Adding sparklines to player page

--- app/models/Sparkline.scala
+++ app/models/Sparkline.scala
@@ -1,3 +1,14 @@
 package models
 
-case class Sparkline(val percentiles: Percentiles)
+case class Sparkline(val percentiles: Percentiles) {
+	val percList = Measurables.all.map(percentiles.getPercentile(_)).flatten
+
+  val xmax = 400
+  val ymax = 100
+
+  def polygon = {
+    "0,0 " + percList.zipWithIndex.map(_ match {
+      case(p, index) => (xmax / percList.length * index) + "," + (ymax * p / 100)
+    }).mkString(" ") + " " + xmax + ",0"
+  }
+}
